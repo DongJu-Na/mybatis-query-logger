fcb814f51eba7b9024083cd49daf234252db1fb5
DongJu-Na <79893048+DongJu-Na@users.noreply.github.com>
2025-06-04 16:00:22 +0900

    init

--- src/main/java/com/ndj/mybatis/logger/QueryLoggerAutoConfiguration.java
+++ src/main/java/com/ndj/mybatis/logger/QueryLoggerAutoConfiguration.java
@@ -1,6 +1,7 @@
 package com.ndj.mybatis.logger;
 
 import org.apache.ibatis.plugin.Interceptor;
+import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -16,5 +17,10 @@ public class QueryLoggerAutoConfiguration {
         QueryLoggerInterceptor interceptor = new QueryLoggerInterceptor();
         interceptor.setLoggerProperties(loggerProperties);
         return interceptor;
+    }
+
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer(Interceptor queryLoggerInterceptor) {
+        return configuration -> configuration.addInterceptor(queryLoggerInterceptor);
     }
 }
